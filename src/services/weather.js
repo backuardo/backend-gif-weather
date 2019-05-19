c7caadc80f9222106ef8f50dbf62dcd02be5666f
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-05-19 09:59:34 -0400

    get hourly forecast in api

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,35 +1,46 @@
 import axios from 'axios';
-import moment from 'moment';
 import dotenv from 'dotenv';
 
 dotenv.config({ silent: true });
 
 const ROOT_URL = 'https://api.darksky.net/forecast';
-const EXCLUSIONS = 'minutely,hourly,alerts,flags';
+const EXCLUSIONS = 'minutely,alerts,flags';
 
 // eslint-disable-next-line import/prefer-default-export
 const getWeather = (lat, lng) => {
   const { DARKSKY_KEY } = process.env;
 
   return new Promise((resolve, reject) => {
-    axios.get(`${ROOT_URL}/${DARKSKY_KEY}/${lat},${lng}?exclude=${EXCLUSIONS}`)
+    axios.get(`${ROOT_URL}/${DARKSKY_KEY}/${lat},${lng}?exclude=${EXCLUSIONS}?units=auto`)
       .then((response) => {
-        const curr = response.data.currently;
-        const { daily } = response.data;
+        const {
+          timezone, currently, hourly, daily,
+        } = response.data;
+        console.log(timezone);
         const result = {
           currently: {
-            time: moment(curr.time, 'X').format('dddd, MM/DD/YYYY, h:mm:ss a'),
-            summary: curr.summary,
-            icon: curr.icon,
-            temperature: curr.temperature,
-            humidity: curr.humidity,
+            summary: currently.summary,
+            icon: currently.icon,
+            temperature: currently.temperature,
+            humidity: currently.humidity,
+          },
+          hourly: {
+            summary: hourly.summary,
+            data: hourly.data.slice(1, 14).map((time) => {
+              return {
+                time: time.time,
+                summary: time.summary,
+                icon: time.icon,
+                temperature: time.temperature,
+              };
+            }),
           },
           daily: {
             summary: daily.summary,
             icon: daily.icon,
             data: daily.data.map((time) => {
               return {
-                time: moment(time.time, 'X').format('dddd, MM/DD/YYYY'),
+                time: time.time,
                 summary: time.summary,
                 icon: time.icon,
                 high: time.temperatureHigh,
